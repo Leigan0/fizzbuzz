33571dbba6d5e20e6971794904cdaff9c7545a0b
Leigh-ann <leighann.gant@gmail.com>
2017-12-24 13:29:57 +0000

    Tests refactored

--- spec/fizbuzz_spec.rb
+++ spec/fizbuzz_spec.rb
@@ -1,29 +1,15 @@
 require 'fizzbuzz'
-# delete this comment
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" when passed multiple of 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'returns "fizzbuzz" when passed 15' do
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
   it 'returns number when passed number that is not divisible by 3 or 5' do
     expect(fizzbuzz(1)).to eq 1
   end
-  it 'returns number when passed number that is not divisible by 3 or 5' do
-    expect(fizzbuzz(88)).to eq 88
-  end
-  it 'returns "fizz" when passed a multiple of 3' do
-    expect(fizzbuzz([3, 6, 9, 12, 18].sample)).to eq 'fizz'
-  end
-  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
-    expect(fizzbuzz([30,45,300,300].sample)).to eq 'fizzbuzz'
-  end
-  it 'returns "buzz" when passed a multiple of 5' do
-    expect(fizzbuzz([5, 10, 25, 75, 100].sample)).to eq 'buzz'
-  end
-ends
