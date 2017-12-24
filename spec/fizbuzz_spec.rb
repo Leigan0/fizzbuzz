b50185235fdf120897775f6471aa279927739c72
rcvink <rrvink@gmail.com>
2017-12-24 12:28:05 +0000

    Add random tests for multiples of 3 and 5

--- spec/fizbuzz_spec.rb
+++ spec/fizbuzz_spec.rb
@@ -1,11 +1,11 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'return contains fizz when passed a random multiple of 3' do
+    expect(fizzbuzz(3 * rand(100))).to include("fizz")
   end
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  it 'return contains buzz when passed a random multiple of 5' do
+    expect(fizzbuzz(5 * rand(100))).to include("buzz")
   end
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
@@ -16,13 +16,7 @@ describe 'fizzbuzz' do
   it 'returns number when passed number that is not divisible by 3 or 5' do
     expect(fizzbuzz(88)).to eq 88
   end
-  it 'returns "fizz" when passed a multiple of 3' do
-    expect(fizzbuzz([3, 6, 9, 12, 18].sample)).to eq 'fizz'
-  end
   it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
     expect(fizzbuzz([30,45,300,300].sample)).to eq 'fizzbuzz'
-  end
-  it 'returns "buzz" when passed a multiple of 5' do
-    expect(fizzbuzz([5, 10, 25, 75, 100].sample)).to eq 'buzz'
   end
 end
